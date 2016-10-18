8ee723db0e4f27d723fe4a619eaf799d52e2cf78
samidarko <samidarko@gmail.com>
2016-10-18 20:25:23 +0800

    Builder pattern added, target and project directories removed + exclued from git

---
+++ src/main/scala/BuilderPattern.scala
@@ -0,0 +1,6 @@
+/**
+  * Created by vincentdupont on 18/10/16.
+  */
+object BuilderPattern {
+
+}
--- src/main/scala/SingletonPattern.scala
+++ src/main/scala/SingletonPattern.scala
@@ -1,6 +1,12 @@
 /**
   * Created by samidarko on 10/18/16.
+  * What is the Singleton Pattern
+  * * It is used when you want to eliminate the option of instantiation more than one object
+  *
   */
 object SingletonPattern extends App {
-  // TODO SingletonPattern
+  /*
+  In Scala "object" keyword returns a Singletom
+  http://docs.scala-lang.org/tutorials/tour/singleton-objects.html
+   */
 }
